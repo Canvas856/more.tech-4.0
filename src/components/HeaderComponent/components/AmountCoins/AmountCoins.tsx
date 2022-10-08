2a6a1eac92445b0b74dba5a70baa498b6605083a
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-10-08 15:10:58 +0800

    🆕 ручка баланса кошелька

--- src/components/HeaderComponent/components/AmountCoins/AmountCoins.tsx
+++ src/components/HeaderComponent/components/AmountCoins/AmountCoins.tsx
@@ -1,14 +1,17 @@
 import { Flex, Text } from '@chakra-ui/react';
+import { useBalance } from '~/api/balance';
 import { CoinsComponent } from '~/components/CoinsComponent';
 import { colors } from '~/theme/colors';
 
 export const AmountCoins = () => {
+  const { data: balance } = useBalance();
+
   return (
     <Flex flexDirection={'column'} alignItems={'flex-end'}>
       <Text color={colors.brand.blue[500]} fontWeight={'500'} fontSize={'20px'}>
         Баланс coin
       </Text>
-      <CoinsComponent coins={253} />
+      <CoinsComponent coins={balance ? balance.coinsAmount : 0} />
     </Flex>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from 'react-query';
 import theme from './theme';
 import Fonts from './theme/Fonts';
+import { queryClient } from './api/client';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Fonts />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <Fonts />
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 );
