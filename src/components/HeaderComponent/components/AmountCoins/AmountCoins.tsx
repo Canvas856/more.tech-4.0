f85d42fdb197f00107ec6f1e0daaaa62ea4b45f7
Canvas856 <askaroffrusik@yandex.ru>
2022-10-08 23:43:54 +0300

    XAC-19  Добавил Popover

--- src/components/HeaderComponent/components/AmountCoins/AmountCoins.tsx
+++ src/components/HeaderComponent/components/AmountCoins/AmountCoins.tsx
@@ -11,8 +11,11 @@ import {
   Portal,
   Text,
 } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import { useBalance } from '~/api/balance';
 import { RewardComponent } from '~/components/RewardComponent';
+import Router from '~/router/Router';
+import { Routes } from '~/router/routes';
 import { colors } from '~/theme/colors';
 
 const userImageUrl = new URL('./user.svg', import.meta.url).href;
@@ -35,8 +38,21 @@ export const AmountCoins = () => {
             <PopoverContent>
               <PopoverArrow />
               <PopoverCloseButton />
-              <PopoverBody>
-                <Button colorScheme='blue'>Button</Button>
+              <PopoverBody
+                padding={'10px'}
+                display={'flex'}
+                bgColor={colors.brand.black}
+                flexDirection={'column'}
+              >
+                <Link to={Routes.profile} color={colors.brand.white}>
+                  Личный кабинет
+                </Link>
+                <Link to={Routes.wallet} color={colors.brand.white}>
+                  Мой кошелек
+                </Link>
+                <Link to={''} color={colors.brand.white}>
+                  Выйти из аккаунта
+                </Link>
               </PopoverBody>
             </PopoverContent>
           </Portal>
