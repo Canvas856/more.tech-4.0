53be081a1ca255ac7e8cbfbfb5b7ddfac81fa89d
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-10-09 14:10:18 +0800

    🆕 сверстал выпадающее меню навигации

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,14 +1,97 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Portal,
+  Flex,
+  Image,
+  Box,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { Link, To } from 'react-router-dom';
 import { AmountCoins } from './components/AmountCoins';
 import { Links } from './components/Links';
 import { LogoVTB } from './components/LogoVTB';
+import { Routes } from '~/router/routes';
+import { colors } from '~/theme/colors';
+
+const userImageUrl = new URL('./user.svg', import.meta.url).href;
+
+const DropdownLink: React.FC<{
+  children: React.ReactNode;
+  to: To;
+  borderBottom?: string;
+  onClick?: () => void;
+}> = ({ children, to, borderBottom = '', onClick }) => (
+  <Text
+    as={Link}
+    onClick={onClick}
+    fontSize='16px'
+    fontWeight='300'
+    color={colors.brand.white}
+    to={to}
+    borderBottom={borderBottom}
+    pt='10px'
+    pb='10px'
+    sx={{ '&:hover': { color: colors.brand.blue[100] } }}
+  >
+    {children}
+  </Text>
+);
+
+const DropdownComponent = () => {
+  const { isOpen, onToggle, onClose } = useDisclosure();
+
+  return (
+    <Popover isOpen={isOpen} onClose={onClose}>
+      <PopoverTrigger>
+        <Box as='button' onClick={onToggle} pl='20px' pr='20px'>
+          <Image w='50px' h='50px' cursor='pointer' alt='user' src={userImageUrl} />
+        </Box>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent
+          maxWidth='250px'
+          bg={colors.brand.blue[700]}
+          borderRadius='20px'
+          p='10px 20px'
+          border='none'
+        >
+          <DropdownLink
+            onClick={onClose}
+            to={Routes.profile}
+            borderBottom={`1px solid ${colors.brand.black}`}
+          >
+            Личный кабинет
+          </DropdownLink>
+          <DropdownLink
+            onClick={onClose}
+            to={Routes.wallet}
+            borderBottom={`1px solid ${colors.brand.black}`}
+          >
+            Мой кошелек
+          </DropdownLink>
+          <DropdownLink onClick={onClose} to={''}>
+            Выйти из аккаунта
+          </DropdownLink>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  );
+};
 
 export const HeaderComponent = () => {
   return (
-    <Flex justifyContent={'space-between'}>
+    <Flex>
       <LogoVTB />
-      <Links />
-      <AmountCoins />
+      <Box m='auto 0 auto auto'>
+        <Links />
+      </Box>
+      <Box ml='auto' mr='20px'>
+        <AmountCoins />
+      </Box>
+      <DropdownComponent />
     </Flex>
   );
 };
--- src/components/HeaderComponent/components/AmountCoins/AmountCoins.tsx
+++ src/components/HeaderComponent/components/AmountCoins/AmountCoins.tsx
@@ -1,24 +1,8 @@
-import {
-  Button,
-  Flex,
-  Image,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverTrigger,
-  Portal,
-  Text,
-} from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Flex, Text } from '@chakra-ui/react';
 import { useBalance } from '~/api/balance';
 import { RewardComponent } from '~/components/RewardComponent';
-import Router from '~/router/Router';
-import { Routes } from '~/router/routes';
-import { colors } from '~/theme/colors';
 
-const userImageUrl = new URL('./user.svg', import.meta.url).href;
+import { colors } from '~/theme/colors';
 
 export const AmountCoins = () => {
   const { data: balance } = useBalance();
@@ -30,33 +14,6 @@ export const AmountCoins = () => {
       </Text>
       <Flex gap={'40px'}>
         <RewardComponent type='coin' reward={balance ? balance.coinsAmount : 0} />
-        <Popover>
-          <PopoverTrigger>
-            <Image alt='user' src={userImageUrl} />
-          </PopoverTrigger>
-          <Portal>
-            <PopoverContent>
-              <PopoverArrow />
-              <PopoverCloseButton />
-              <PopoverBody
-                padding={'10px'}
-                display={'flex'}
-                bgColor={colors.brand.black}
-                flexDirection={'column'}
-              >
-                <Link to={Routes.profile} color={colors.brand.white}>
-                  Личный кабинет
-                </Link>
-                <Link to={Routes.wallet} color={colors.brand.white}>
-                  Мой кошелек
-                </Link>
-                <Link to={''} color={colors.brand.white}>
-                  Выйти из аккаунта
-                </Link>
-              </PopoverBody>
-            </PopoverContent>
-          </Portal>
-        </Popover>
       </Flex>
     </Flex>
   );
